adbf2bd9e2959b1bdaf90b3c882f3a7c074c3a67
lclc98 <liam.c14785@gmail.com>
2014-06-05 20:25:37 +1000

    Fixed Fog

--- src/main/java/com/teammetallurgy/atum/lib/tickhandler/ClientTickHandler.java
+++ src/main/java/com/teammetallurgy/atum/lib/tickhandler/ClientTickHandler.java
@@ -21,70 +21,70 @@ public class ClientTickHandler {
 	public static int defaultFog;
 
 	@SubscribeEvent
-	public void onUpdate(TickEvent.PlayerTickEvent event) {
-		EntityPlayer player = event.player;
-		if (event.type.equals(event.type.PLAYER) && event.phase.equals(event.phase.START)) {
+	public void onRender(TickEvent.RenderTickEvent event) {
+		EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+		ItemStack currentItemStack = null;
 
-			boolean nightvision = false;
+		if (player != null && player.getCurrentArmor(3) != null) {
+			if (player.getCurrentArmor(3).getItem() == AtumItems.mummyHelmet) {
+				ScaledResolution scaledresolution = new ScaledResolution(Minecraft.getMinecraft().gameSettings, Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
+				int par1 = scaledresolution.getScaledWidth();
+				int par2 = scaledresolution.getScaledHeight();
 
-			if (player.getCurrentArmor(3) != null) {
-				if (player.getCurrentArmor(3).getItem() == AtumItems.rasGlory) {
-					nightvision = true;
-				}
-
-			}
-
-			if (player.dimension == AtumIDS.DIMENSION_ID) {
-				if (Minecraft.getMinecraft().gameSettings.renderDistanceChunks < (nightvision ? 1 : 2)) {
-					defaultFog = Minecraft.getMinecraft().gameSettings.renderDistanceChunks;
-					Minecraft.getMinecraft().gameSettings.renderDistanceChunks = nightvision ? 1 : 2;
-				}
-
-				if (player.worldObj.isRaining()) {
-					raining = true;
-					if (Minecraft.getMinecraft().gameSettings.renderDistanceChunks < (nightvision ? 2 : 3)) {
-						Minecraft.getMinecraft().gameSettings.renderDistanceChunks = nightvision ? 2 : 3;
-					}
-
-					Random random = new Random();
-					int particlesPerTick = (3 - Minecraft.getMinecraft().gameSettings.particleSetting) * 6;
-					for (int i = 0; i < particlesPerTick; i++) {
-						float x = random.nextInt(4) - 2;
-						float z = random.nextInt(4) - 2;
-						float y = (random.nextFloat() - 0.7F) * 2F;
-
-						float vx = 0.1F + random.nextFloat() * 0.1F;
-						float vz = 0.1F + random.nextFloat() * 0.1F;
-
-						player.worldObj.spawnParticle("sand", player.posX + x, player.posY + y, player.posZ + z, vx + player.motionX, 0.0D, vz + player.motionZ);
-					}
-				} else {
-					if (raining == true && defaultFog < (nightvision ? 1 : 2)) {
-						raining = false;
-						Minecraft.getMinecraft().gameSettings.renderDistanceChunks = nightvision ? 1 : 2;
-
-					}
-				}
-
+				Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("atum", "textures/hud/mummyblur.png"));
+				Tessellator tessellator = Tessellator.instance;
+				tessellator.startDrawingQuads();
+				tessellator.addVertexWithUV(0.0D, par2, -100, 0.0D, 1.0D);
+				tessellator.addVertexWithUV(par1, par2, -100, 1.0D, 1.0D);
+				tessellator.addVertexWithUV(par1, 0.0D, -100, 1.0D, 0.0D);
+				tessellator.addVertexWithUV(0.0D, 0.0D, -100, 0.0D, 0.0D);
+				tessellator.draw();
 			}
 		}
-		if (event.type.equals(event.type.RENDER) && event.phase.equals(event.phase.END)) {
-			ItemStack currentItemStack = null;
+	}
 
-			if (player != null && player.getCurrentArmor(3) != null) {
-				if (player.getCurrentArmor(3).getItem() == AtumItems.mummyHelmet) {
-					ScaledResolution scaledresolution = new ScaledResolution(Minecraft.getMinecraft().gameSettings, Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
-					int par1 = scaledresolution.getScaledWidth();
-					int par2 = scaledresolution.getScaledHeight();
+	@SubscribeEvent
+	public void onUpdate(TickEvent.PlayerTickEvent event) {
+		EntityPlayer player = event.player;
 
-					Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("atum", "textures/hud/mummyblur.png"));
-					Tessellator tessellator = Tessellator.instance;
-					tessellator.startDrawingQuads();
-					tessellator.addVertexWithUV(0.0D, par2, -100, 0.0D, 1.0D);
-					tessellator.addVertexWithUV(par1, par2, -100, 1.0D, 1.0D);
-					tessellator.addVertexWithUV(par1, 0.0D, -100, 1.0D, 0.0D);
-					tessellator.addVertexWithUV(0.0D, 0.0D, -100, 0.0D, 0.0D);
-					tessellator.draw();
+		boolean nightvision = false;
+
+		if (player.getCurrentArmor(3) != null) {
+			if (player.getCurrentArmor(3).getItem() == AtumItems.rasGlory) {
+				nightvision = true;
+			}
+
+		}
+
+		if (player.dimension == AtumIDS.DIMENSION_ID) {
+			if (Minecraft.getMinecraft().gameSettings.renderDistanceChunks > 2) {
+				defaultFog = Minecraft.getMinecraft().gameSettings.renderDistanceChunks;
+				Minecraft.getMinecraft().gameSettings.renderDistanceChunks = nightvision ? 1 : 2;
+			}
+
+			if (player.worldObj.isRaining()) {
+				raining = true;
+				if (Minecraft.getMinecraft().gameSettings.renderDistanceChunks < (nightvision ? 3 : 2)) {
+					Minecraft.getMinecraft().gameSettings.renderDistanceChunks = nightvision ? 3 : 2;
+				}
+
+				Random random = new Random();
+				int particlesPerTick = (3 - Minecraft.getMinecraft().gameSettings.particleSetting) * 6;
+				for (int i = 0; i < particlesPerTick; i++) {
+					float x = random.nextInt(4) - 2;
+					float z = random.nextInt(4) - 2;
+					float y = (random.nextFloat() - 0.7F) * 2F;
+
+					float vx = 0.1F + random.nextFloat() * 0.1F;
+					float vz = 0.1F + random.nextFloat() * 0.1F;
+
+					player.worldObj.spawnParticle("sand", player.posX + x, player.posY + y, player.posZ + z, vx + player.motionX, 0.0D, vz + player.motionZ);
+				}
+			} else {
+				if (raining == true && defaultFog < (nightvision ? 1 : 2)) {
+					raining = false;
+					Minecraft.getMinecraft().gameSettings.renderDistanceChunks = nightvision ? 3 : 2;
+
 				}
 			}
 		}
