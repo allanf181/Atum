ea341c076d6c832838945b5ef6f9a39b820cb315
jaredlll08 <jaredlll08@gmail.com>
2015-10-20 21:08:38 +0200

    Close #54 Close #51

--- src/main/java/com/teammetallurgy/atum/blocks/ItemBlockSlab.java
+++ src/main/java/com/teammetallurgy/atum/blocks/ItemBlockSlab.java
@@ -1,18 +1,35 @@
 package com.teammetallurgy.atum.blocks;
 
+import java.util.List;
+
 import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 public class ItemBlockSlab extends ItemBlock {
-    public static final String[] types = {"smooth", "cracked", "largeBrick", "smallBrick"};
+	public static final String[] types = { "smooth", "cracked", "largeBrick", "smallBrick" };
 
-    public ItemBlockSlab(Block block) {
-        super(block);
-    }
+	public ItemBlockSlab(Block block) {
+		super(block);
+		setHasSubtypes(true);
+	}
 
-    @Override
-    public String getUnlocalizedName(ItemStack par1ItemStack) {
-        return "tile." + types[par1ItemStack.getItemDamage()] + "Slab";
-    }
+	@Override
+	public void getSubItems(Item item, CreativeTabs tab, List list) {
+		for (int i = 0; i < types.length; i++) {
+			list.add(new ItemStack(item, 1, i));
+		}
+	}
+
+	@Override
+	public int getMetadata(int meta) {
+		return meta;
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack par1ItemStack) {
+		return "tile." + types[par1ItemStack.getItemDamage()] + "Slab";
+	}
 }
--- src/main/java/com/teammetallurgy/atum/blocks/ItemBlockWall.java
+++ src/main/java/com/teammetallurgy/atum/blocks/ItemBlockWall.java
@@ -1,19 +1,38 @@
 package com.teammetallurgy.atum.blocks;
 
+import java.util.List;
+
 import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 public class ItemBlockWall extends ItemBlock {
-    public static final String[] types = {"smooth", "cracked", "largeBrick", "smallBrick"};
+	public static final String[] types = { "smooth", "cracked", "largeBrick", "smallBrick" };
 
-    public ItemBlockWall(Block block) {
-        super(block);
-    }
+	public ItemBlockWall(Block block) {
+		super(block);
+		setHasSubtypes(true);
+	}
 
-    @Override
-    public String getUnlocalizedName(ItemStack par1ItemStack) {
-        return "tile." + types[par1ItemStack.getItemDamage()] + "Wall";
-    }
+	@Override
+	public int getMetadata(int meta) {
+		return meta;
+	}
+	
+	@Override
+	public void getSubItems(Item item, CreativeTabs tab, List list) {
+		for(int i = 0;i<types.length;i++){
+			list.add(new ItemStack(item, 1, i));
+		}
+	}
+	
+	
+
+	@Override
+	public String getUnlocalizedName(ItemStack par1ItemStack) {
+		return "tile." + types[par1ItemStack.getItemDamage()] + "Wall";
+	}
 
 }
