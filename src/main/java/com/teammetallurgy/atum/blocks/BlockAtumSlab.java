628b5577a629a0435610ed5b69b700648585a0d6
jaredlll08 <jaredlll08@gmail.com>
2015-10-21 01:26:01 +0200

    format fix for Slab

--- src/main/java/com/teammetallurgy/atum/blocks/BlockAtumSlab.java
+++ src/main/java/com/teammetallurgy/atum/blocks/BlockAtumSlab.java
@@ -1,7 +1,6 @@
 package com.teammetallurgy.atum.blocks;
 
 import com.teammetallurgy.atum.Atum;
-
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockSlab;
@@ -15,62 +14,46 @@ import net.minecraft.util.IIcon;
 import java.util.List;
 
 public class BlockAtumSlab extends BlockSlab {
-	public static final String[] slabType = { "smooth", "cracked", "largeBrick", "smallBrick" };
+    public static final String[] slabType = {"smooth", "cracked", "largeBrick", "smallBrick"};
 
-	public BlockAtumSlab(boolean par2) {
-		super(par2, Material.rock);
-		this.setHardness(2.0F);
-		this.setCreativeTab(Atum.creativeTab);
-		setLightOpacity(1);
-	}
+    public BlockAtumSlab(boolean par2) {
+        super(par2, Material.rock);
+        this.setHardness(2.0F);
+        this.setCreativeTab(Atum.creativeTab);
+    }
 
-	@Override
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int par1, int par2) {
-		par2 %= 4;
-		if (par2 == 0)
-			return AtumBlocks.BLOCK_STONE.getIcon(par1, par2 & 0x7);
-		if (par2 == 1)
-			return AtumBlocks.BLOCK_LIMESTONECOBBLE.getIcon(par1, par2 & 0x7);
-		if (par2 == 2) {
-			return AtumBlocks.BLOCK_LARGEBRICK.getIcon(par1, par2 & 0x7);
-		}
-		return AtumBlocks.BLOCK_SMALLBRICK.getIcon(par1, par2 & 0x7);
-	}
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int par1, int par2) {
+        par2 %= 4;
+        if (par2 == 0)
+            return AtumBlocks.BLOCK_STONE.getIcon(par1, par2 & 0x7);
+        if (par2 == 1)
+            return AtumBlocks.BLOCK_LIMESTONECOBBLE.getIcon(par1, par2 & 0x7);
+        if (par2 == 2) {
+            return AtumBlocks.BLOCK_LARGEBRICK.getIcon(par1, par2 & 0x7);
+        }
+        return AtumBlocks.BLOCK_SMALLBRICK.getIcon(par1, par2 & 0x7);
+    }
 
-	@Override
-	protected ItemStack createStackedBlock(int par1) {
-		return new ItemStack(this, 2, par1 & 0x7);
-	}
+    @Override
+    protected ItemStack createStackedBlock(int par1) {
+        return new ItemStack(this, 2, par1 & 0x7);
+    }
 
-	@Override
-	public boolean renderAsNormalBlock() {
-		return false;
-	}
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item par1, CreativeTabs tab, List subItems) {
+        for (int i = 0; i < 4; i++)
+            subItems.add(new ItemStack(this, 1, i));
+    }
 
-	@Override
-	public boolean isOpaqueCube() {
-		return false;
-	}
+    @Override
+    public String func_150002_b(int var1) {
+        return this.getUnlocalizedName();
+    }
 
-	@Override
-	public boolean isNormalCube() {
-		return false;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void getSubBlocks(Item par1, CreativeTabs tab, List subItems) {
-		for (int i = 0; i < 4; i++)
-			subItems.add(new ItemStack(this, 1, i));
-	}
-
-	@Override
-	public String func_150002_b(int var1) {
-		return this.getUnlocalizedName();
-	}
-
-	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister p_149651_1_) {
-	}
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister p_149651_1_) {
+    }
 }
