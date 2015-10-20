7aedb4cf3116f2af8d4b0da1cd2b25dfd4988bd8
jaredlll08 <jaredlll08@gmail.com>
2015-10-21 01:37:47 +0200

    More format

--- src/main/java/com/teammetallurgy/atum/blocks/ItemBlockSlab.java
+++ src/main/java/com/teammetallurgy/atum/blocks/ItemBlockSlab.java
@@ -9,27 +9,26 @@ import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 public class ItemBlockSlab extends ItemBlock {
-	public static final String[] types = { "smooth", "cracked", "largeBrick", "smallBrick" };
+    public static final String[] types = {"smooth", "cracked", "largeBrick", "smallBrick"};
 
-	public ItemBlockSlab(Block block) {
-		super(block);
-		setHasSubtypes(true);
-	}
+    public ItemBlockSlab(Block block) {
+        super(block);
+        setHasSubtypes(true);
+    }
+    @Override
+    public void getSubItems(Item item, CreativeTabs tab, List list) {
+    	for(int i = 0;i< types.length;i++){
+    		list.add(new ItemStack(this, 1, i));
+    	}
+    }
+    
+    @Override
+    public int getMetadata(int meta) {
+    	return meta;
+    }
 
-	@Override
-	public void getSubItems(Item item, CreativeTabs tab, List list) {
-		for (int i = 0; i < types.length; i++) {
-			list.add(new ItemStack(this, 1, i));
-		}
-	}
-
-	@Override
-	public int getMetadata(int meta) {
-		return meta;
-	}
-
-	@Override
-	public String getUnlocalizedName(ItemStack par1ItemStack) {
-		return "tile." + types[par1ItemStack.getItemDamage()] + "Slab";
-	}
+    @Override
+    public String getUnlocalizedName(ItemStack par1ItemStack) {
+        return "tile." + types[par1ItemStack.getItemDamage()] + "Slab";
+    }
 }
