3dfd19a46e363bd8a2948b2503ce6f9c0d9b56a1
GirafiStudios <girafi0@gmail.com>
2015-10-25 04:23:47 +0100

    Disabled Warlord spawn until 0.8 Fixed possible issue with fertile soil

--- src/main/java/com/teammetallurgy/atum/world/biome/BiomeGenAtumDesert.java
+++ src/main/java/com/teammetallurgy/atum/world/biome/BiomeGenAtumDesert.java
@@ -12,7 +12,7 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 
 import java.util.Random;
 
-public class BiomeGenAtumDesert extends BiomeGenBase {
+public class  BiomeGenAtumDesert extends BiomeGenBase {
 
     public WorldGenerator treeGenerator;
     public WorldGenerator palmGenerator;
@@ -41,7 +41,6 @@ public class BiomeGenAtumDesert extends BiomeGenBase {
         this.setTemperatureRainfall(2.0F, 0.0F);
         this.setHeight(new Height(0.1F, 0.2F));
         super.spawnableMonsterList.add(new SpawnListEntry(EntityMummy.class, 6, 4, 4));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityBanditWarlord.class, 1, 1, 1));
         super.spawnableMonsterList.add(new SpawnListEntry(EntityBanditWarrior.class, 6, 2, 2));
         super.spawnableMonsterList.add(new SpawnListEntry(EntityBarbarian.class, 2, 4, 4));
         super.spawnableMonsterList.add(new SpawnListEntry(EntityBanditArcher.class, 6, 4, 4));
