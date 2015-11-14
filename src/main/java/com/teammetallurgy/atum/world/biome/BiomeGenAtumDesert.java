aac202d24757a5cf81c152865da039c279abcf67
Ammon Lauritzen <allaryin@gmail.com>
2015-11-14 00:35:50 -0600

    Created parent class with biome defaults.

--- src/main/java/com/teammetallurgy/atum/world/biome/BiomeGenAtumDesert.java
+++ src/main/java/com/teammetallurgy/atum/world/biome/BiomeGenAtumDesert.java
@@ -12,34 +12,21 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 
 import java.util.Random;
 
-public class BiomeGenAtumDesert extends BiomeGenBase {
+public class BiomeGenAtumDesert extends AtumBiomeGenBase {
 
     public WorldGenerator treeGenerator;
     public WorldGenerator palmGenerator;
     public WorldGenerator ruinsGenerator;
 
-    public BiomeGenAtumDesert(int par1) {
-        super(par1);
-        super.spawnableCreatureList.clear();
-        this.topBlock = AtumBlocks.BLOCK_SAND;
-        this.fillerBlock = AtumBlocks.BLOCK_STONE;
+    public BiomeGenAtumDesert(int biomeID) {
+        super(biomeID);
+        
+        this.setBiomeName("Atum's Desert");
 
-        super.theBiomeDecorator.treesPerChunk = 1;
-        super.theBiomeDecorator.deadBushPerChunk = 5;
-        super.theBiomeDecorator.reedsPerChunk = 0;
-        super.theBiomeDecorator.cactiPerChunk = 0;
         this.treeGenerator = new WorldGenAtumTrees(true);
         this.palmGenerator = new WorldGenPalm(true);
         this.ruinsGenerator = new WorldGenRuins();
-        super.spawnableMonsterList.clear();
-        super.spawnableCreatureList.clear();
-        super.spawnableWaterCreatureList.clear();
-        super.spawnableCaveCreatureList.clear();
-        this.setColor(16421912);
-        this.setBiomeName("Atum's Desert");
-        this.setDisableRain();
-        this.setTemperatureRainfall(2.0F, 0.0F);
-        this.setHeight(new Height(0.1F, 0.2F));
+        
         super.spawnableMonsterList.add(new SpawnListEntry(EntityMummy.class, 6, 4, 4));
         super.spawnableMonsterList.add(new SpawnListEntry(EntityBanditWarrior.class, 6, 2, 2));
         super.spawnableMonsterList.add(new SpawnListEntry(EntityBarbarian.class, 2, 4, 4));
@@ -49,11 +36,6 @@ public class BiomeGenAtumDesert extends BiomeGenBase {
         super.spawnableMonsterList.add(new SpawnListEntry(EntityDesertWolf.class, 4, 1, 4));
         super.spawnableMonsterList.add(new SpawnListEntry(EntityStoneSoldier.class, 6, 4, 4));
         super.spawnableMonsterList.add(new SpawnListEntry(EntityBonestorm.class, 6, 4, 4));
-    }
-
-    @Override
-    public BiomeDecorator createBiomeDecorator() {
-        return new BiomeDecoratorAtum();
     }
 
     @Override
