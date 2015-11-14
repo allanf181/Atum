434018c7342f33fd2f147176059f3258329e5670
Ammon Lauritzen <allaryin@gmail.com>
2015-11-14 01:15:47 -0600

    Pulled default spawnable mob registration into a method.

--- src/main/java/com/teammetallurgy/atum/world/biome/AtumBiomeGenBase.java
+++ src/main/java/com/teammetallurgy/atum/world/biome/AtumBiomeGenBase.java
@@ -1,11 +1,21 @@
 package com.teammetallurgy.atum.world.biome;
 
 import com.teammetallurgy.atum.blocks.AtumBlocks;
+import com.teammetallurgy.atum.entity.EntityBanditArcher;
+import com.teammetallurgy.atum.entity.EntityBanditWarrior;
+import com.teammetallurgy.atum.entity.EntityBarbarian;
+import com.teammetallurgy.atum.entity.EntityBonestorm;
+import com.teammetallurgy.atum.entity.EntityDesertWolf;
+import com.teammetallurgy.atum.entity.EntityDustySkeleton;
+import com.teammetallurgy.atum.entity.EntityGhost;
+import com.teammetallurgy.atum.entity.EntityMummy;
+import com.teammetallurgy.atum.entity.EntityStoneSoldier;
 import com.teammetallurgy.atum.handler.AtumConfig;
 
 import net.minecraft.world.biome.BiomeDecorator;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.BiomeGenBase.Height;
+import net.minecraft.world.biome.BiomeGenBase.SpawnListEntry;
 
 public class AtumBiomeGenBase extends BiomeGenBase {
 
@@ -32,6 +42,18 @@ public class AtumBiomeGenBase extends BiomeGenBase {
 		
         this.setBiomeName(config.toString());
         config.setGen(this);
+	}
+	
+	protected void addDefaultSpawns() {
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityMummy.class, 6, 4, 4));
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityBanditWarrior.class, 6, 2, 2));
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityBarbarian.class, 2, 4, 4));
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityBanditArcher.class, 6, 4, 4));
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityDustySkeleton.class, 6, 4, 4));
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityGhost.class, 6, 4, 4));
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityDesertWolf.class, 4, 1, 4));
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityStoneSoldier.class, 6, 4, 4));
+        super.spawnableMonsterList.add(new SpawnListEntry(EntityBonestorm.class, 6, 4, 4));
 	}
 
     @Override
--- src/main/java/com/teammetallurgy/atum/world/biome/BiomeGenAtumDesert.java
+++ src/main/java/com/teammetallurgy/atum/world/biome/BiomeGenAtumDesert.java
@@ -25,15 +25,7 @@ public class BiomeGenAtumDesert extends AtumBiomeGenBase {
         this.palmGenerator = new WorldGenPalm(true);
         this.ruinsGenerator = new WorldGenRuins();
         
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityMummy.class, 6, 4, 4));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityBanditWarrior.class, 6, 2, 2));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityBarbarian.class, 2, 4, 4));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityBanditArcher.class, 6, 4, 4));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityDustySkeleton.class, 6, 4, 4));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityGhost.class, 6, 4, 4));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityDesertWolf.class, 4, 1, 4));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityStoneSoldier.class, 6, 4, 4));
-        super.spawnableMonsterList.add(new SpawnListEntry(EntityBonestorm.class, 6, 4, 4));
+        super.addDefaultSpawns();
     }
 
     @Override
@@ -65,7 +57,7 @@ public class BiomeGenAtumDesert extends AtumBiomeGenBase {
             k = par3 + par2Random.nextInt(16) + 8;
             l = par4 + par2Random.nextInt(16) + 8;
             height = par2Random.nextInt(4) + 4;
-            (new WorldGenAtumTrees(true)).generate(par1World, par2Random, k, par1World.getHeightValue(k, l), l);
+            this.treeGenerator.generate(par1World, par2Random, k, par1World.getHeightValue(k, l), l);
         }
 
         if (par2Random.nextInt(240) == 0) {
